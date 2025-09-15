e8a37b5252892cadedee489c494de317d813842d
James Rhamy <1060896+Akidi@users.noreply.github.com>
2025-09-15 16:37:01 -0500

    test current eventsystem / logging

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script>
+	import { gEvents } from "$lib/eventsystem";
+	import { logger } from "$lib/logging";
+  gEvents.on("game.stateChangeRequest", payload => {
+    logger.info("events", "Received game.stateChangeRequest", payload);
+  })
+</script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<button onclick={() => gEvents.emit("game.stateChangeRequest", {newState: "main_menu"})}>Main Menu</button>
