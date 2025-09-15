7c91389f27f9c765f67defa711eb9f8519219a32
James Rhamy <1060896+Akidi@users.noreply.github.com>
2025-09-15 16:32:53 -0500

    initial commit

---
+++ src/lib/shared/AppEvents.ts
@@ -0,0 +1,12 @@
+import type { ValidState } from "./ValidStates";
+
+export type StateChangeRejectionReason = 'invalid_state' | 'invalid_transition';
+
+export interface AppEvents {
+  'game.stateChanged': { previousState: ValidState, newState: ValidState },
+  'game.stateChangeRequest': { newState: ValidState},
+  'game.stateChangeStarted': { previousState: ValidState, requestedState: ValidState },
+  'game.stateChangeRejected': { previousState: ValidState, requestedState: ValidState, reason: StateChangeRejectionReason, message?: string },
+}
+
+export type AppEventName = keyof AppEvents;
